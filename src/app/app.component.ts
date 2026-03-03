bf894ec55280bd7366a8107775bf710fa43952a2
Arif Ali <arifali22290@gmail.com>
2026-03-03 16:31:24 -0500

    Section 2: Angular Fundamentals Interpolation and Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'MyAngularApp';
-  a = 10;
-  b = 20;
-  ans = this.a + this.b;
+
+  inputValue: string = 'Hello';
+  isDisabled: boolean = false;
+
+  imgSrc: string = "/assets/Angular.png";
 }
